95177787f9a6baedeaf2779df0cfde9192720078
Matheus Oliveira <matheus0mco@gmail.com>
2018-08-04 14:39:58 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testeLeituraTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
